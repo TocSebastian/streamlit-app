ec38bcac01a3502bfdf981db18b5d419f83eda2a
TocSebastian <91591126+TocSebastian@users.noreply.github.com>
2022-04-19 22:50:48 +0300

    Update nav_bar.py

--- nav_bar.py
+++ nav_bar.py
@@ -17,7 +17,7 @@ nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-img = Image.open('logo.svg.webp')
+img = Image.open('logo.svg.png')
 
 #make it look nice from the start
 st.set_page_config(page_title = 'Movies IMDb',page_icon=img, layout='wide',initial_sidebar_state='collapsed',)
